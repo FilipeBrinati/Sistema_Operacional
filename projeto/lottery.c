cc4c1dbeced0db74b1bddad6f818727ad59e8623
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2022-07-18 21:03:31 -0300

    implementado lottInitSchedInfo

--- projeto/lottery.c
+++ projeto/lottery.c
@@ -1,65 +1,79 @@
 /*
-*  lottery.c - Implementacao do algoritmo Lottery Scheduling e sua API
-*
-*  Autores: SUPER_PROGRAMADORES_C
-*  Projeto: Trabalho Pratico I - Sistemas Operacionais
-*  Organizacao: Universidade Federal de Juiz de Fora
-*  Departamento: Dep. Ciencia da Computacao
-*
-*/
+ *  lottery.c - Implementacao do algoritmo Lottery Scheduling e sua API
+ *
+ *  Autores: SUPER_PROGRAMADORES_C
+ *  Projeto: Trabalho Pratico I - Sistemas Operacionais
+ *  Organizacao: Universidade Federal de Juiz de Fora
+ *  Departamento: Dep. Ciencia da Computacao
+ *
+ */
 
 #include "lottery.h"
 #include <stdio.h>
 #include <string.h>
 
-//Nome unico do algoritmo. Deve ter 4 caracteres.
-const char lottName[]="LOTT";
+// Nome unico do algoritmo. Deve ter 4 caracteres.
+const char lottName[] = "LOTT";
 //=====Funcoes Auxiliares=====
-
-
-
-
-
 
 //=====Funcoes da API=====
 
-//Funcao chamada pela inicializacao do S.O. para a incializacao do escalonador
-//conforme o algoritmo Lottery Scheduling
-//Deve envolver a inicializacao de possiveis parametros gerais
-//Deve envolver o registro do algoritmo junto ao escalonador
-void lottInitSchedInfo() {
+// Funcao chamada pela inicializacao do S.O. para a incializacao do escalonador
+// conforme o algoritmo Lottery Scheduling
+// Deve envolver a inicializacao de possiveis parametros gerais
+// Deve envolver o registro do algoritmo junto ao escalonador
+void lottInitSchedInfo()
+{
+	SchedInfo *si = malloc(sizeof(SchedInfo));
+
+	int i;
+	for (i = 0; i < 4; i++)
+	{
+		si->name[i] = lottName[i];
+	}
+
+	si->initParamsFn = &lottInitSchedParams;
+	si->notifyProcStatusChangeFn = &lottNotifyProcStatusChange;
+	si->scheduleFn = &lottSchedule;
+	si->releaseParamsFn = &lottReleaseParams;
+
+	schedRegisterScheduler(si);
+}
+
+// Inicializa os parametros de escalonamento de um processo p, chamada
+// normalmente quando o processo e' associado ao slot de Lottery
+void lottInitSchedParams(Process *p, void *params)
+{
 	//...
 }
 
-//Inicializa os parametros de escalonamento de um processo p, chamada 
-//normalmente quando o processo e' associado ao slot de Lottery
-void lottInitSchedParams(Process *p, void *params) {
+// Recebe a notificação de que um processo sob gerência de Lottery mudou de estado
+// Deve realizar qualquer atualização de dados da Loteria necessária quando um processo muda de estado
+void lottNotifyProcStatusChange(Process *p)
+{
 	//...
 }
 
-//Recebe a notificação de que um processo sob gerência de Lottery mudou de estado
-//Deve realizar qualquer atualização de dados da Loteria necessária quando um processo muda de estado
-void lottNotifyProcStatusChange(Process* p) {
-	//...
-}
-
-//Retorna o proximo processo a obter a CPU, conforme o algortimo Lottery 
-Process* lottSchedule(Process *plist) {
+// Retorna o proximo processo a obter a CPU, conforme o algortimo Lottery
+Process *lottSchedule(Process *plist)
+{
 	//...
 	return NULL;
 }
 
-//Libera os parametros de escalonamento de um processo p, chamada 
-//normalmente quando o processo e' desassociado do slot de Lottery
-//Retorna o numero do slot ao qual o processo estava associado
-int lottReleaseParams(Process *p) {
+// Libera os parametros de escalonamento de um processo p, chamada
+// normalmente quando o processo e' desassociado do slot de Lottery
+// Retorna o numero do slot ao qual o processo estava associado
+int lottReleaseParams(Process *p)
+{
 	//...
 	return 0;
 }
 
-//Transfere certo numero de tickets do processo src para o processo dst.
-//Retorna o numero de tickets efetivamente transfeirdos (pode ser menos)
-int lottTransferTickets(Process *src, Process *dst, int tickets) {
+// Transfere certo numero de tickets do processo src para o processo dst.
+// Retorna o numero de tickets efetivamente transfeirdos (pode ser menos)
+int lottTransferTickets(Process *src, Process *dst, int tickets)
+{
 	//...
 	return 0;
 }
